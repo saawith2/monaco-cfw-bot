8c6267040e52a1d10b207cef86f0fc0af04fd976
kaf7 saawith <bbbnjjsj@gmail.com>
2026-05-12 13:11:18 +0300

    Add files via upload

---
+++ on.js
@@ -0,0 +1,19 @@
+# ⚠️ انسخ هذا الملف باسم .env وأضف بيانات حقيقية
+
+# توكن البوت من https://discord.com/developers/applications
+BOT_TOKEN=your_bot_token_here_xxxxxxxxxxxxxxxxx
+
+# معرف التطبيق من https://discord.com/developers/applications
+CLIENT_ID=1234567890123456789
+
+# معرف السيرفر (يمكن تعيينه لاحقاً عبر /set-guild)
+GUILD_ID=1234567890123456789
+
+# منفذ Keep-alive (لـ Railway و Heroku)
+PORT=3000
+
+# ملاحظات:
+# - BOT_TOKEN: لا تشارك هذا مع أحد!
+# - CLIENT_ID: معرف تطبيقك في Discord
+# - GUILD_ID: معرف السيرفر الخاص بك
+# - PORT: عادة 3000 أو 8080
